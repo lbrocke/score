9c8dee645e4624cf7f2f0e6ff960c051b32742ce
Lukas Brocke <lukas@brocke.net>
2023-04-20 00:38:15 +0200

    Implement validation of Match

--- parser/parser.go
+++ parser/parser.go
@@ -2,13 +2,21 @@ package parser
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"time"
 )
 
 const (
-	ERR_INVALID_JSON  = "JSON is invalid."
-	ERR_INVALID_MATCH = "Match is invalid."
+	ERR_INVALID_JSON    = "JSON is invalid"
+	ERR_INVALID_MATCH   = "match is invalid"
+	ERR_INVALID_MODE    = "mode is invalid"
+	ERR_INVALID_COUNTRY = "country is invalid"
+	ERR_INVALID_NAME    = "name is invalid"
+	ERR_INVALID_TEAMS   = "teams are invalid"
+	ERR_INVALID_TIMES   = "times are invalid"
+	ERR_INVALID_POINT   = "point is invalid"
+	ERR_INVALID_GAME    = "game is invalid"
 )
 
 func max(a int, b int) int {
@@ -35,15 +43,23 @@ const (
 )
 
 const (
-	Mode11WinPoints  = 11
-	Mode11TiePoints  = 15
-	Mode11WinSets    = 3
-	Mode11WinMaxSets = 5
+	Mode11WinPoints = 11
+	Mode11TiePoints = 15
+	Mode11WinGames  = 3
+	Mode11MaxGames  = 5
 
-	Mode21WinPoints  = 21
-	Mode21TiePoints  = 30
-	Mode21WinSets    = 2
-	Mode21WinMaxSets = 3
+	Mode21WinPoints = 21
+	Mode21TiePoints = 30
+	Mode21WinGames  = 2
+	Mode21MaxGames  = 3
+)
+
+type Country string
+
+const (
+	CountryDE = "DE"
+	CountryDK = "DK"
+	CountryTW = "TW"
 )
 
 func numWinPoints(mode Mode) int {
@@ -51,9 +67,9 @@ func numWinPoints(mode Mode) int {
 	case Mode11:
 		return Mode11WinPoints
 	case Mode21:
-		fallthrough
-	default:
 		return Mode21WinPoints
+	default:
+		return -1
 	}
 }
 
@@ -62,9 +78,31 @@ func numTiePoints(mode Mode) int {
 	case Mode11:
 		return Mode11TiePoints
 	case Mode21:
-		fallthrough
-	default:
 		return Mode21TiePoints
+	default:
+		return -1
+	}
+}
+
+func numWinGames(mode Mode) int {
+	switch mode {
+	case Mode11:
+		return Mode11WinGames
+	case Mode21:
+		return Mode21WinGames
+	default:
+		return -1
+	}
+}
+
+func numMaxGames(mode Mode) int {
+	switch mode {
+	case Mode11:
+		return Mode11MaxGames
+	case Mode21:
+		return Mode21MaxGames
+	default:
+		return -1
 	}
 }
 
@@ -107,25 +145,153 @@ type Match struct {
 	Team2GamePoints int       `json:"-"`
 }
 
+func isValidMode(mode Mode) bool {
+	return mode == Mode11 || mode == Mode21
+}
+
+func isValidCountry(country string) bool {
+	switch country {
+	case CountryDE:
+		fallthrough
+	case CountryDK:
+		fallthrough
+	case CountryTW:
+		return true
+	default:
+		return false
+	}
+}
+
+func isValidName(name string) bool {
+	return len(name) != 0
+}
+
+func count(slice []Team, team Team) int {
+	cnt := 0
+
+	for _, val := range slice {
+		if val == team {
+			cnt++
+		}
+	}
+
+	return cnt
+}
+
+func validateGames(games []Game, mode Mode) error {
+	winPoints := numWinPoints(mode)
+	tiePoints := numTiePoints(mode)
+	winGames := numWinGames(mode)
+	maxGames := numMaxGames(mode)
+
+	var winner []Team
+
+	if len(games) > maxGames {
+		return errors.New(ERR_INVALID_GAME)
+	}
+
+	for i, game := range games {
+		scoreTeam1 := 0
+		scoreTeam2 := 0
+
+		for j, point := range game.Points {
+			// check if point was given to either Team1 or Team2
+			if !(point == Team1 || point == Team2) {
+				return errors.New(ERR_INVALID_POINT)
+			}
+
+			if point == Team1 {
+				scoreTeam1++
+			} else if point == Team2 {
+				scoreTeam2++
+			}
+
+			// check if Team1 has won
+			if scoreTeam1 == tiePoints || (scoreTeam1 >= winPoints && scoreTeam1-scoreTeam2 >= 2) {
+				winner = append(winner, Team1)
+
+				// check if points were counted afterwards
+				if j != len(game.Points)-1 {
+					return errors.New(ERR_INVALID_POINT)
+				}
+
+				break
+			}
+
+			// check if Team2 has won
+			if scoreTeam2 == tiePoints || (scoreTeam2 >= winPoints && scoreTeam2-scoreTeam1 >= 2) {
+				winner = append(winner, Team1)
+
+				// check if points were counted afterwards
+				if j != len(game.Points)-1 {
+					return errors.New(ERR_INVALID_POINT)
+				}
+
+				break
+			}
+		}
+
+		// If there is no winner for this game yet, then the game is still running and no later game must exist
+		if len(winner) < i+1 && len(games) > i+1 {
+			return errors.New(ERR_INVALID_GAME)
+		}
+
+		numWinsTeam1 := count(winner, Team1)
+		numWinsTeam2 := count(winner, Team2)
+
+		// If there is a winner for this match, then no later games must exist
+		if (numWinsTeam1 == winGames && numWinsTeam2 < winGames || numWinsTeam2 == winGames && numWinsTeam1 < winGames) &&
+			(len(games) > i+1) {
+			return errors.New(ERR_INVALID_GAME)
+		}
+	}
+
+	return nil
+}
+
 func (match *Match) validate() error {
-	//   Validate Mode
-	//   Validate Country
-	//   Validate Name not empty
-	//   Validate Points
+	if !isValidMode(match.Info.Mode) {
+		return errors.New(ERR_INVALID_MODE)
+	}
+
+	for _, player := range match.Info.Team1 {
+		if !isValidCountry(player.Country) {
+			return errors.New(ERR_INVALID_COUNTRY)
+		}
+
+		if !isValidName(player.Player) {
+			return errors.New(ERR_INVALID_NAME)
+		}
+	}
+
+	for _, player := range match.Info.Team2 {
+		if !isValidCountry(player.Country) {
+			return errors.New(ERR_INVALID_COUNTRY)
+		}
+
+		if !isValidName(player.Player) {
+			return errors.New(ERR_INVALID_NAME)
+		}
+	}
+
+	if !(len(match.Info.Team1) == len(match.Info.Team2) &&
+		(len(match.Info.Team1) == 1 || len(match.Info.Team1) == 2)) {
+		return errors.New(ERR_INVALID_TEAMS)
+	}
+
+	if !match.Info.Start.Time.Before(match.Info.End.Time) {
+		return errors.New(ERR_INVALID_TIMES)
+	}
+
+	if err := validateGames(match.Games, match.Info.Mode); err != nil {
+		return err
+	}
 
 	return nil
 }
 
 func calculatePointsInGame(points []Team, team Team) int {
-	count := 0
-
-	for _, s := range points {
-		if s == team {
-			count++
-		}
-	}
-
-	return count
+	return count(points, team)
 }
 
 // Calculates the maximum of consecutive points the given team has
@@ -202,7 +368,7 @@ func calculatePointsWonInMatch(games []Game, team Team) int {
 	for _, game := range games {
 		if team == Team1 {
 			sum += game.Team1PointsWon
-		} else {
+		} else if team == Team2 {
 			sum += game.Team2PointsWon
 		}
 	}
@@ -216,7 +382,7 @@ func calculateConsecutivePointsInMatch(games []Game, team Team) int {
 	for _, game := range games {
 		if team == Team1 {
 			maxCons = max(maxCons, game.Team1ConsPoints)
-		} else {
+		} else if team == Team2 {
 			maxCons = max(maxCons, game.Team2ConsPoints)
 		}
 	}
@@ -230,7 +396,7 @@ func calculateGamePointsInMatch(games []Game, team Team) int {
 	for _, game := range games {
 		if team == Team1 {
 			sum += game.Team1GamePoints
-		} else {
+		} else if team == Team2 {
 			sum += game.Team2GamePoints
 		}
 	}
@@ -239,15 +405,13 @@ func calculateGamePointsInMatch(games []Game, team Team) int {
 }
 
 func calculateGamesWonInMatch(games []Game, team Team) int {
-	won := 0
+	winners := []Team{}
 
 	for _, game := range games {
-		if game.Winner == team {
-			won++
-		}
+		winners = append(winners, game.Winner)
 	}
 
-	return won
+	return count(winners, team)
 }
 
 // Calculates game/match winners and statistics such as consecutive points won,
@@ -267,10 +431,15 @@ func (m *Match) determineStats() {
 
 		m.Games[i].PointsPlayed = m.Games[i].Team1PointsWon + m.Games[i].Team2PointsWon
 
-		if m.Games[i].Team1PointsWon > m.Games[i].Team2PointsWon {
+		scoreTeam1 := m.Games[i].Team1PointsWon
+		scoreTeam2 := m.Games[i].Team2PointsWon
+
+		if scoreTeam1 == numTiePoints(m.Info.Mode) || (scoreTeam1 >= numWinPoints(m.Info.Mode) && scoreTeam1-scoreTeam2 >= 2) {
 			m.Games[i].Winner = Team1
-		} else {
+		} else if scoreTeam2 == numTiePoints(m.Info.Mode) || (scoreTeam2 >= numWinPoints(m.Info.Mode) && scoreTeam2-scoreTeam1 >= 2) {
 			m.Games[i].Winner = Team2
+		} else {
+			m.Games[i].Winner = Unknown
 		}
 	}
 
@@ -284,10 +453,12 @@ func (m *Match) determineStats() {
 	m.Team2ConsPoints = calculateConsecutivePointsInMatch(m.Games, Team2)
 	m.Team2GamePoints = calculateGamePointsInMatch(m.Games, Team2)
 
-	if calculateGamesWonInMatch(m.Games, Team1) > calculateGamesWonInMatch(m.Games, Team2) {
+	if calculateGamesWonInMatch(m.Games, Team1) == numWinGames(m.Info.Mode) {
 		m.Winner = Team1
-	} else {
+	} else if calculateGamesWonInMatch(m.Games, Team2) == numWinGames(m.Info.Mode) {
 		m.Winner = Team2
+	} else {
+		m.Winner = Unknown
 	}
 }
 
@@ -300,7 +471,7 @@ func Parse(data string, validate bool) (Match, error) {
 
 	if validate {
 		if err := match.validate(); err != nil {
-			return match, fmt.Errorf("%s %s", ERR_INVALID_MATCH, err.Error())
+			return match, fmt.Errorf("%s: %s", ERR_INVALID_MATCH, err.Error())
 		}
 	}
 
